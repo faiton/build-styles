f8c62cc2fba0eb42227d56562a5dba871a7b891d
Mushex Antaranian <mushex@antaranian.com>
2014-08-14 05:59:50 +0400

    add some tests

---
+++ test/index.js
@@ -0,0 +1,40 @@
+var assert = require('assert');
+
+var join = require('path').join;
+
+var resolve = require('component-resolver');
+
+var Build = require('./build.js');
+
+describe('example', function () {
+    var fixture = join(__dirname, 'fixtures/example'),
+        build, css;
+
+    it('should resolve', function (done) {
+        resolve(fixture, {
+            install: true,
+        }, function (err, tree) {
+            if (err) return done(err);
+
+            build = Build(tree);
+            done();
+        });
+    });
+
+    it('should build styles', function (done) {
+        build.styles(function (err, string) {
+            if (err) return done(err);
+
+            css = string;
+            done();
+        });
+    });
+
+    it('should autoprefix', function (){
+        assert(~css.indexOf('-webkit-flex'));
+    });
+
+    it('should post-process with Rework', function(){
+        assert(~css.indexOf('color:#ff0084'));
+    });
+});
